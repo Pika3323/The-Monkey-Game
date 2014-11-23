18999811510d92bf2c0811b5f8e363d59b8267eb
Pika3323 <derekellis170@yahoo.ca>
2014-11-22 22:33:12 -0500

    Color Selector Menu
    A working color selector menu under the scene "Color_Menu"
    
    +1 for inconsistant use of "Colour" and "Color"

--- Scripts/colourSelect.py
+++ Scripts/colourSelect.py
@@ -8,44 +8,43 @@ xC = 1
 yC = 1
 x = own.worldPosition.x
 y = own.worldPosition.z
+dc = "Color_White"
 print(own.worldPosition)
 def main():
     global xC
     global yC
     global x
     global y
+    global dc
     key = cont.sensors['Keyboard'].events
-    print(key)
     if key ==[[100,1]]:
         if xC < 4:
-            print(xC)
             nX = x + 1.42056
             own.worldPosition.x = nX
             x = own.worldPosition.x
             xC = xC + 1
     elif key ==[[97,1]]:
         if xC > 1:
-            print(xC)
             nX = x - 1.42056
             own.worldPosition.x = nX 
             x = own.worldPosition.x
             xC = xC - 1
     elif key ==[[119,1]]:
         if yC > 1:
-            print(yC)
             nY = y + 1.42056
             own.worldPosition.z = nY
             y = own.worldPosition.z
             yC = yC - 1
     elif key ==[[115,1]]:
         if yC < 4:
-            print(yC)
             nY = y - 1.42056
-            print(nY)
             own.worldPosition.z = nY
             y = own.worldPosition.z
-            print(y)
             yC = yC + 1
     else:
         pass
-    
+    print(colours.colour[xC - 1][yC - 1])
+    print("Y: " + str(yC))
+    nc = colours.colour[xC - 1][yC - 1]
+    scene.objects['Suzanne_Color_White'].replaceMesh('Suzanne_' + nc, True, False)
+    dc = nc
