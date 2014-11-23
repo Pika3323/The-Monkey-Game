9be4cff05159dfb7a478436cd10dad4f52a2ef65
Pika3323 <derekellis170@yahoo.ca>
2014-11-23 18:48:59 -0500

    Colour Selector 80% complete

--- Scripts/colourSelect.py
+++ Scripts/colourSelect.py
@@ -3,48 +3,95 @@ import colours
 
 cont = bge.logic.getCurrentController()
 scene = bge.logic.getCurrentScene()
-own = cont.owner
+
+p1 = scene.objects['Selector_1']
+p2 = scene.objects['Selector_2']
 xC = 1
 yC = 1
-x = own.worldPosition.x
-y = own.worldPosition.z
+x = p1.worldPosition.x
+y = p1.worldPosition.z
+xC2 = 1
+yC2 = 1
+x2 = p2.worldPosition.x
+y2 = p2.worldPosition.z
 dc = "Color_White"
-def main():
+dc2 = "Color_White"
+def p1f():
     global xC
     global yC
     global x
     global y
     global dc
-    key = cont.sensors['Keyboard'].events
-    print(key)
+    key = p1.sensors['Keyboard'].events
     if key ==[[100,1]]:
         if xC < 4:
             nX = x + 1.42056
-            own.worldPosition.x = nX
-            x = own.worldPosition.x
+            p1.worldPosition.x = nX
+            x = p1.worldPosition.x
             xC = xC + 1
     elif key ==[[97,1]]:
         if xC > 1:
             nX = x - 1.42056
-            own.worldPosition.x = nX 
-            x = own.worldPosition.x
+            p1.worldPosition.x = nX 
+            x = p1.worldPosition.x
             xC = xC - 1
     elif key ==[[119,1]]:
         if yC > 1:
             nY = y + 1.42056
-            own.worldPosition.z = nY
-            y = own.worldPosition.z
+            p1.worldPosition.z = nY
+            y = p1.worldPosition.z
             yC = yC - 1
     elif key ==[[115,1]]:
         if yC < 4:
             nY = y - 1.42056
-            own.worldPosition.z = nY
-            y = own.worldPosition.z
+            p1.worldPosition.z = nY
+            y = p1.worldPosition.z
             yC = yC + 1
     else:
         pass
-    on = own.name
-    lt = own.name[-2:]
     nc = colours.colour[xC - 1][yC - 1]
-    scene.objects['Suzanne' + lt].replaceMesh('Suzanne_' + nc, True, False)
+    scene.objects['Suzanne_1'].replaceMesh('Suzanne_' + nc, True, False)
     dc = nc
+def p2f():
+    global xC2
+    global yC2
+    global x2
+    global y2
+    global dc2
+    key2 = p2.sensors['Keyboard'].events
+    if key2 ==[[145,1]]:
+        if xC2 < 4:
+            nX2 = x2 + 1.42056
+            p2.worldPosition.x = nX2
+            x2 = p2.worldPosition.x
+            xC2 = xC2 + 1
+    elif key2 ==[[143,1]]:
+        if xC2 > 1:
+            nX2 = x2 - 1.42056
+            p2.worldPosition.x = nX2 
+            x2 = p2.worldPosition.x
+            xC2 = xC2 - 1
+    elif key2 ==[[146,1]]:
+        if yC2 > 1:
+            nY2 = y2 + 1.42056
+            p2.worldPosition.z = nY2
+            y2 = p2.worldPosition.z
+            yC2 = yC2 - 1
+    elif key2 ==[[144,1]]:
+        if yC2 < 4:
+            nY2 = y2 - 1.42056
+            p2.worldPosition.z = nY2
+            y2 = p2.worldPosition.z
+            yC2 = yC2 + 1
+    else:
+        pass
+    nc2 = colours.colour[xC2 - 1][yC2 - 1]
+    scene.objects['Suzanne_2'].replaceMesh('Suzanne_' + nc2, True, False)
+    dc2 = nc2
+def newP():
+    contr = bge.logic.getCurrentController()
+    ownr = contr.owner
+    scene.objects['Suzanne_2'].visible = True
+    scene.objects['Selector_2'].visible = True
+    ownr.visible = False
+    
